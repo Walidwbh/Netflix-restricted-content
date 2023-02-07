6abd7007539b9555ef2cd5fcbcb431fb4e109904
walidwbh <93353066+Walidwbh@users.noreply.github.com>
2023-02-07 22:52:27 +0100

    work is done

--- js/main.js
+++ js/main.js
@@ -1,19 +1,45 @@
 const inputs = Array.from(document.querySelectorAll("input"));
-let password = "";
+let password = [];
 new Promise((resolve, reject)=>{
-inputs.forEach((element)=>{
+inputs.forEach((element, index)=>{
     element.addEventListener("input",(event)=>{
-        password+=event.target.value;
-        if(event.target.value.length>0 & element.nextElementSibling!=null){
+        if(event.target.value.length>0 & index < inputs.length-1){
             event.target.blur();
             element.nextElementSibling.focus();
-        }else{
+            password.push(event.target.value)
+        }else if(index == inputs.length - 1){
             event.target.blur();
-            resolve();
+            password.push(event.target.value)
+            //when we finish we check if password true we resolve if not we reject
+            if(password.join("")=="1234"){
+                resolve();
+            }else{
+                reject()
+            }
+        }else if(event.inputType == "deleteContentBackward" & index > 0) {
+            event.target.blur();
+            element.previousElementSibling.focus();
+            password.pop()
+        }else {
+            event.target.blur();
+            password.pop()
         }
     });
 })}).then(()=>{
-    if(password!=="1234"){
-        document.querySelector(".pin .title").textContent="Incorrect PIN. Please try again.";
-    }
+    let i = 5;
+    let interval = setInterval((e)=>{
+        i--;
+        document.querySelector(".pin .title").textContent=`Correct PIN, Please wait ${i} seconds to be redirected`;
+        if(i == 0 ){
+            clearInterval(interval);
+            document.querySelector(".pin").style.display = "none";
+            document.getElementById("background").classList.remove("background");
+        }
+    },1000);
+}).catch(()=>{
+    document.querySelector(".pin .title").textContent="Incorrect PIN. Please try again.";
+    inputs.forEach((e)=>{
+        e.value = ""
+    });
+    inputs[0].focus();
 })
